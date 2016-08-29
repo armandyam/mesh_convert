92b37ea91baef43ebff3f8157240d7c55b0f8a57
Dan Ibanez <dan.a.ibanez@gmail.com>
2016-08-29 11:04:05 -0400

    remove trailing whitespace

--- netgen_read.cpp
+++ netgen_read.cpp
@@ -26,13 +26,13 @@ int main(int argc, char* argv[])
   }
   int mtris, nvmax;
   inmesh >> mtris;
-  
+
   int tripoints[mtris][3];
   nvmax=1;
   i = 0;
   while (i < mtris)
   {
-    inmesh >> dummy >> dummy >> dummy >> dummy >> dummy >> tripoints[i][0] >> tripoints[i][1] >> tripoints[i][2]; 
+    inmesh >> dummy >> dummy >> dummy >> dummy >> dummy >> tripoints[i][0] >> tripoints[i][1] >> tripoints[i][2];
     nvmax=max(max(tripoints[i][0], tripoints[i][1]), max(tripoints[i][2],nvmax));
 
     i++;
@@ -46,7 +46,7 @@ int main(int argc, char* argv[])
   int medges;
   inmesh >> medges;
   int epoints[medges][2], ednr1[medges], ednr2[medges], surfid[medges];
-  double dist1[medges], dist2[medges]; 
+  double dist1[medges], dist2[medges];
   int v_surfid[nvmax];
   for (i = 0; i < nvmax; ++i) v_surfid[i] = 0;
   {
@@ -55,11 +55,11 @@ int main(int argc, char* argv[])
   while (i < medges)
   {
     inmesh >> surfid[i] >> dummy >> epoints[i][0] >> epoints[i][1] >> dummy >> dummy >> dummy >> dummy >> ednr1[i] >> dist1[i] >> ednr2[i] >> dist2[i];
-    if (dist1[i] == 0 ) 
+    if (dist1[i] == 0 )
     {
-       v_surfid[epoints[i][0] - 1] = epoints[i][0]; 
+       v_surfid[epoints[i][0] - 1] = epoints[i][0];
     }
-    else 
+    else
     {
        v_surfid[epoints[i][0] - 1] = surfid[i];
     }
